e4b5da55e8d510e6e2b9eb2b10d06e58a7b03b5d
itsmeabhishekn <abhishekn4454@gmail.com>
2022-12-05 22:11:00 +0530

    added home page

--- libraryBackend/src/main/java/com/example/libraryBackend/controller/librarycontroller.java
+++ libraryBackend/src/main/java/com/example/libraryBackend/controller/librarycontroller.java
@@ -1,5 +1,6 @@
 package com.example.libraryBackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,13 @@ public class librarycontroller {
     {
         return "this is the login page";
     }
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "this is the home page";
+    }
+
+
+
 }
